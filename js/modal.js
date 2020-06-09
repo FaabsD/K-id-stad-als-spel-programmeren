814473fd4b25e7d8fc42bcbcc8f1363f04571dc7
Fabian <19959@ma-web.nl>
2020-06-09 18:33:48 +0200

    script om modaal venster te maken bij tik op afbeelding

---
+++ js/modal.js
@@ -0,0 +1,46 @@
+const alleInformatie = document.querySelectorAll('.hint');
+const alleThumbnails = document.querySelectorAll('.image-holder');
+
+let inhoud = [];
+for(let i=0; i<alleInformatie.length; i++){
+    inhoud.push(alleInformatie[i]);
+    alleInformatie[i].remove();
+}
+
+function deleteModal(){
+    document.getElementById('modal-window').remove();
+}
+
+function makeModal(num) {
+    console.log(inhoud[num]);
+    // Maak een modal venster
+    let modalWindow = document.createElement('div');
+    modalWindow.className = 'modal-window';
+    modalWindow.id = 'modal-window';
+    modalWindow.addEventListener('click', deleteModal);
+    // maak een sluitknop
+    let closeBtn = document.createElement('span');
+    closeBtn.className = 'close-button';
+    closeBtn.innerHTML = '&#8855;';
+    //voeg een eventlistener toe aan de sluitknop
+    closeBtn.addEventListener('click', deleteModal);
+    // maak een container voor de inhoud van het modal venster
+    let modalContent = document.createElement('div');
+    modalContent.className = 'modal-content';
+    modalContent.innerHTML = inhoud[num].innerHTML;
+    modalContent.addEventListener('click', function(e){
+        e.stopPropagation();
+    });
+    gsap.to(modalContent, {marginTop:0, duration: 1, ease: "elastic.out(1, 0.3)"});
+    modalContent.prepend(closeBtn);
+    // Wijs de modal container toe aan het modal venster
+    modalWindow.append(modalContent);
+    // wijs het modaal venster toe aan de body
+    document.body.append(modalWindow);
+}
+
+for (let i=0; i<alleThumbnails.length; i++) {
+    alleThumbnails[i].addEventListener('click', function() {
+        makeModal(i)
+    })
+}
